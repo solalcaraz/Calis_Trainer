2628e1ce3d424a945090cb6a153d60766fd016ea
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-28 19:42:15 -0300

    cards clases en home

---
+++ src/lib/components/home/clasesCard/clases.ts
@@ -0,0 +1,51 @@
+export class Clase {
+  constructor(
+    public id: string,
+    public icono: string,
+    public nombre: string,
+    public breveDescripcion: string,
+    public duracion: string,
+    public detalles: string[],
+    public ubicacion: string,
+    public descripcion: string
+  ){}
+}
+
+export const clases = [
+  new Clase('presencial', 
+    'src/lib/assets/users-three.svg', 
+    'Clases Presenciales', 
+    'Entrená en equipo al aire libre.',
+    '1hs',
+    [
+      "Lunes 18:30 a 21:00",
+      "Miércoles 19:00 a 21:00",
+      "Viernes 18:30 a 21:00"
+    ],
+    'Plaza Irlanda - Caballito',
+    'Entrenamientos grupales al aire libre en un ambiente motivador y en contacto con la naturaleza. Las progresiones están adaptadas a todos los niveles, desde principiantes hasta avanzados. Cada sesión combina movimiento, fuerza y energía compartida.'
+  ),
+  new Clase('online', 
+    'src/lib/assets/laptop.svg', 
+    'Clases Online', 
+    'Accedé a entrenamientos guiados desde tu casa.',
+    'Plan de 4 semanas',
+    [
+      "Plan de entrenamiento personalizado",
+      "Llamadas de acompañamiento semanales",
+      "Videos explicativos",
+      "Seguimiento constante"
+    ],
+    'Virtual',
+    'Entrená desde tu casa con rutinas claras, apoyo constante y resultados reales. Ideal si buscás flexibilidad horaria y mantener la motivación con un acompañamiento cercano.'
+  ),
+  new Clase('personalizado', 
+    'src/lib/assets/handshake.svg', 
+    'Clases Personalizadas', 
+    'Sesiones 1 a 1 para progresar a tu ritmo.',
+    'Sesiones 1 a 1 (consultar disponibilidad)',
+    [],
+    'En CABA',
+    'Entrenamiento individual adaptado 100% a tus objetivos: calistenia o fitness integral. Sesiones diseñadas para mejorar fuerza, técnica y resultados, con acompañamiento cercano y feedback directo.'
+  )
+]
---
+++ src/routes/clases/+page.svelte
@@ -0,0 +1,60 @@
+<!-- 
+ Nombre de la clase y descripción completa
+
+Horarios y calendario (opcional, calendario visual es muy útil)
+
+Ubicación o modalidad (presencial/online)
+
+Precio o plan de clases (si aplica)
+
+Botón: Contactar por WhatsApp
+-->
+<script>
+  import { clases } from '$lib/home/clasesCard/clases.ts'
+</script>
+<main>
+  <h1>Clases</h1>
+  <p> Descubrí la propuesta que mejor se adapta a vos.  
+    Todas las clases están pensadas para acompañarte en tu progreso, sea en grupo, online o de manera personalizada.
+  </p>
+
+  {#each clases as clase}
+    <section id={clase.id}>
+      <h2>{clase.nombre}</h2>
+      <p>{clase.detalle}</p>
+  
+    <!-- horarios y descripción -->
+    <!-- 
+      1hs
+      Horarios:
+      lunes 18.30 a 21hs
+      miercoles 19 a 21hs
+      viernes 18.30 a 21hs
+      Entrenamientos al aire libre en Plaza irlanda - caballito
+      Clases grupales en contacto con la naturaleza, energía motivadora y progresiones adaptadas a todos los niveles.
+    -->
+  </section>
+
+  <section id="online">
+    <h2>Clases Online</h2>
+    <!-- info online -->
+    <!-- 
+      Clases online de calistenia con seguimiento personalizado durante 4 semanas
+      Incluye:
+      plan de entrenamiento adaptado para vos
+      llamadas de acompañamiento
+      videos explicativos
+      Entrena desde tu cada con progresiones claras, apoyo constante y resultados reales.
+    -->
+  </section>
+
+  <section id="personalizadas">
+    <h2>Clases Personalizadas</h2>
+    <!-- info personalizada -->
+    <!-- 
+      Entrenamiento individual en CABA, adaptado a tus objetivos (calistenia o fitness integral)
+      Sesiones 1 a 1 para mejorar fuerza, tecnica y resultados, con acompañamiento cercano
+      Consultar horarios disponibles.
+    -->
+  </section>
+</main>
