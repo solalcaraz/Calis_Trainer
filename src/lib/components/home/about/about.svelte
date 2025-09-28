ba2f941d01ad86d383254426b3a2cd7068326ce6
Sol Alcaraz <alcarazmsol@gmail.com>
2025-09-28 16:11:42 -0300

    fix about

--- src/lib/components/home/about/about.svelte
+++ src/lib/components/home/about/about.svelte
@@ -2,19 +2,26 @@
   import './about.css'
   // import imagen from ...
 </script>
-<section class="contenedor-general">
-  <div class="about--contenedor">
-    <div class="about_contenedor--imagen">
-      <!-- <img class="foto" src={} alt="Entrenador de calistenia"> -->
-    </div>
-    <div>
-      <h2>Te cuento un poco quien soy...</h2>
+<section class="contenedor-general about--contenedor">
+  <div class="about_contenedor--imagen">
+    <img class="foto" src="/calis-trainer-about.jpg" alt="Entrenadora de calistenia">
+  </div>
+  <div class="contenedor_general--contenido contenido--especifico">
+    <h2>La edad no es un límite, es un desafío</h2>
+    <div class="about_contenedor--texto">
       <p>
-        Soy entrenadora de calistenia apasionada por el movimiento libre y el 
-        desarrollo progresivo de la fuerza. Mi objetivo es ayudar a las personas 
-        a descubrir todo lo que su cuerpo puede lograr sin necesidad de máquinas, 
-        solo con su propio peso.
+        Hace 3 años decidí romper la creencia más limitante de todas: la edad.
+        “Soy muy grande o muy chico”, “ya no es para mí”, “pasó mi época”... incluso yo, con 20 años, me sentía así.
       </p>
-    </div>
+      <p>
+        Hasta que llegué a la calistenia, que me desafió y me enamoró. En un año me demostré que podía lograr mucho, fisíca y mentalmente.
+      </p>
+      <p>
+        Aprendí que la edad no es un impedimento: ni en deporte, ni en relaciones, ni en sueños.
+        Confié en las personas que vinieron a mis clases, y juntas descubrimos que todos pueden lograrlo, a su ritmo y sin presiones.  
+      </p>
+      <p>
+        Hoy sé que la mayoría de quienes pasaron por mis clases ya no son los mismos: cambiaron su estilo de vida, su manera de pensar… y eso me llena el corazón ♥️
+      </p>
   </div>
 </section>
