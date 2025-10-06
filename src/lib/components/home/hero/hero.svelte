78ff87568e28a3fd7fb3da723ce091411c7843f3
Sol Alcaraz <alcarazmsol@gmail.com>
2025-10-06 14:05:12 -0300

    fix num de wpp para hero

--- src/lib/components/home/hero/hero.svelte
+++ src/lib/components/home/hero/hero.svelte
@@ -1,7 +1,12 @@
 <script>
   import './hero.css'
-
   import Boton from '$lib/components/generales/boton/boton.svelte'
+
+  const accesoWpp = () => {
+    const mensaje = encodeURIComponent("¡Hola! Me gustaría sumarme al equipo de calistenia")
+    const numero = "5491137568867"
+    window.open(`https://wa.me/${numero}?text=${mensaje}`, "_blank", "noopener,noreferrer")
+  }
 </script>
 
 <section class="hero">
@@ -10,7 +15,7 @@
     <h2>Entrenadora de Calistenia</h2>
     <h3>Transformá tu cuerpo y tu mente a cualquier etapa de tu vida</h3>
   </article>
-  <Boton tipo='primario'>
+  <Boton tipo='primario' onclick={() => accesoWpp()}>
     Sumate al equipo
   </Boton>
 </section>
