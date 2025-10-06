0cb0877b62397cc1c31181e11a0591d83c5cde92
Sol Alcaraz <alcarazmsol@gmail.com>
2025-10-06 14:03:57 -0300

    add component modal

---
+++ src/lib/components/generales/modal/modal.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import './modal.css'
+  import type { Snippet } from 'svelte'
+
+  interface PropsModal {
+    modalVisible: boolean
+    onClose: () => void
+    titulo: string
+    contenido: Snippet
+  }
+
+  let { modalVisible, onClose, titulo, contenido }: PropsModal = $props()
+
+</script>
+
+{#if modalVisible}
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_static_element_interactions -->
+  <!-- svelte-ignore event_directive_deprecated -->
+  <div class="modal-overlay" on:click={() => onClose()}>
+    <div class="contenedor-general modal-content" on:click|stopPropagation>
+      <button class="modal-close" on:click={() => onClose()}>Cerrar</button>
+      <h3>{titulo}</h3>
+       {@render contenido()}
+    </div>
+  </div>
+{/if}
