963fc74f3c75c24c8c7cc7005e1c61c8639e5393
solalcaraz <alcarazmsol@gmail.com>
2025-10-26 19:39:03 -0300

    implemento hero para clases

---
+++ src/routes/alumnos/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import './vistaAlumnos.css'
+  import Hero from '$lib/components/home/hero/hero.svelte'
+</script>
+
+<Hero
+  h1="Nuestras Clases"
+  vista="clases"
+  h3="Conocé los avances, experiencias y logros de quienes entrenan en el equipo."
+/>
