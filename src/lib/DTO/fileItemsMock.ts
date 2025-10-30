1000ccdd34674d2bf6a36a51c06f295b16ad0171
solalcaraz <alcarazmsol@gmail.com>
2025-10-30 12:28:53 -0300

    mock para los archivos

---
+++ src/lib/DTO/fileItemsMock.ts
@@ -0,0 +1,6 @@
+export interface ArchivosItem {
+    id: number
+    src: string
+    titulo?: string
+    descripcion?: string
+  }
---
+++ src/lib/mocks/archivosClasesMock.ts
@@ -0,0 +1,21 @@
+import type { ArchivosItem } from '$lib/DTO/fileItemsMock'
+
+export const testimonios: ArchivosItem[]= [
+  {id: 0, src: '/testimonio1.jpg' },
+  {id: 1, src: '/testimonio2.jpg' },
+  {id: 2, src: '/testimonio3.jpg' }
+]
+
+export const trucos: ArchivosItem[]= [
+  {id: 0, src: '/videos/trucos1.mp4' },
+  {id: 1, src: '/.mp4' },
+  {id: 2, src: '/.mp4' },
+  {id: 3, src: '/.mp4' }
+]
+
+export const grupales: ArchivosItem[]= [
+  {id: 0, src: '/grupales1.jpg' },
+  {id: 1, src: '/grupales2.jpg' },
+  {id: 2, src: '/grupales3.jpg' },
+  {id: 3, src: '/grupales4.jpg' }
+]
