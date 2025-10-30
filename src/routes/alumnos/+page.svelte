07ed55bb6e2581dfaa546545f88486e1800ab1dc
solalcaraz <alcarazmsol@gmail.com>
2025-10-30 12:29:40 -0300

    carrusel en la page alumnos

--- src/routes/alumnos/+page.svelte
+++ src/routes/alumnos/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import './vistaAlumnos.css'
   import Hero from '$lib/components/home/hero/hero.svelte'
+  import Carrusel from '$lib/components/clases/carrusel/carrusel.svelte'
+  import { testimonios, trucos, grupales } from '$lib/mocks/archivosClasesMock'
 </script>
 
 <Hero
@@ -8,3 +10,18 @@
   vista="clases"
   h3="Conocé los avances, experiencias y logros de quienes entrenan en el equipo."
 />
+
+<article class="contenedor-general">
+  <h2>Testimonios de alumnas que pasaron por mis clases</h2>
+  <Carrusel tipo="imagen" items={testimonios}/>
+</article>
+
+<article class="contenedor-general">
+  <h2>Ellas lograron trucos DESDE CERO</h2>
+  <Carrusel tipo="video" items={trucos}/>
+</article>
+
+<article class="contenedor-general">
+  <h2>Juntas somos mas fuertes</h2>
+  <Carrusel tipo="imagen" items={grupales}/>
+</article>
