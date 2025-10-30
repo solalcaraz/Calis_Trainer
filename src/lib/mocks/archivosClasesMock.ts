3890b1515c4d6df5ce50361318f52a60affac76c
solalcaraz <alcarazmsol@gmail.com>
2025-10-30 12:35:44 -0300

    fix para los archivos

--- src/lib/mocks/archivosClasesMock.ts
+++ src/lib/mocks/archivosClasesMock.ts
@@ -1,21 +1,21 @@
 import type { ArchivosItem } from '$lib/DTO/fileItemsMock'
 
 export const testimonios: ArchivosItem[]= [
-  {id: 0, src: '/testimonio1.jpg' },
-  {id: 1, src: '/testimonio2.jpg' },
-  {id: 2, src: '/testimonio3.jpg' }
+  {id: 0, src: '/testimonios/testimonio1.jpg' },
+  {id: 1, src: '/testimonios/testimonio2.jpg' },
+  {id: 2, src: '/testimonios/testimonio3.jpg' }
 ]
 
 export const trucos: ArchivosItem[]= [
   {id: 0, src: '/videos/trucos1.mp4' },
-  {id: 1, src: '/.mp4' },
-  {id: 2, src: '/.mp4' },
-  {id: 3, src: '/.mp4' }
+  {id: 1, src: '/videos/.mp4' },
+  {id: 2, src: '/videos/.mp4' },
+  {id: 3, src: '/videos/.mp4' }
 ]
 
 export const grupales: ArchivosItem[]= [
-  {id: 0, src: '/grupales1.jpg' },
-  {id: 1, src: '/grupales2.jpg' },
-  {id: 2, src: '/grupales3.jpg' },
-  {id: 3, src: '/grupales4.jpg' }
+  {id: 0, src: '/grupales/grupales1.jpg' },
+  {id: 1, src: '/grupales/grupales2.jpg' },
+  {id: 2, src: '/grupales/grupales3.jpg' },
+  {id: 3, src: '/grupales/grupales4.jpg' }
 ]
